808655aaba80db227ed49f82ee7f82a0c3731b2e
Vincent BASTIONI <vbastion@e2r2p2.42.fr>
2017-05-07 17:13:13 +0200

    [EDIT] Simplified and clarified line_to of iso renderer.

--- __rvp/srcs/renderer_iso.c
+++ __rvp/srcs/renderer_iso.c
@@ -48,19 +48,19 @@ static inline void 		set_delta(t_dims *delta, float *scale,
 	delta->y = BOARD_MAX_HEIGHT + board->alts.x * Z_COEFF * *scale;
 }
 
-/*
+
 static void			line_to(const t_dims pos, const t_dir dir,
 							const t_board *board, const t_imgdata *iptr, 
 							const t_dims *delta_dim, const float scale)
 {
 	t_fvector			v[4];
-	t_vertex			ve[2];
 	int					c[4];
+	t_vertex			ve[2];
 	float 				progress;
 	char 				*addr;
 
 	ve[0] = board->vertex[pos.y][pos.x];
-	ve[1] = board->vertex[pos.y + dir > 0][pos.x + dir != 1];
+	ve[1] = board->vertex[pos.y + (dir > 0)][pos.x + (dir != 1)];
 	v[0] = get_iso_pos(ve[0].pos, scale);
 	v[1] = get_iso_pos(ve[1].pos, scale);
 	c[0] = ve[0].color != -1 ? ve[0].color : col_get(ve[0].pos.z, board->alts);
@@ -80,37 +80,6 @@ static void			line_to(const t_dims pos, const t_dir dir,
 		c[3]++;
 	}
 }
-*/
-
-static void			line_to(const t_vertex f, const t_vertex t,
-								const t_board *board, const t_imgdata *iptr, 
-								const t_dims *delta_dim, const float scale)
-{
-	t_fvector			v[4];
-	int					c[4];
-	float 				progress;
-	char 				*addr;
-
-	v[0] = get_iso_pos(f.pos, scale);
-	v[1] = get_iso_pos(t.pos, scale);
-	c[0] = f.color != -1 ? f.color : col_get(f.pos.z, board->alts);
-	c[1] = t.color != -1 ? t.color : col_get(t.pos.z, board->alts);
-	v[2].a = (v[1].a - v[0].a);
-	v[2].b = (v[1].b - v[0].b);
-	c[2] = (int)(sqrt(v[2].a * v[2].a + v[2].b * v[2].b) * LINE_PRE + 0.5);
-	c[3] = 0;
-	while (c[3] < c[2])
-	{
-		progress = (c[3] / (float)c[2]);
-		v[3].a = (v[0].a + (v[1].a - v[0].a) * progress);
-		v[3].b = (v[0].b + (v[1].b - v[0].b) * progress);
-		addr = (iptr->addr + ((int)(v[3].a + 0.5 + delta_dim->x)) * iptr->bpx
-					+ ((int)(v[3].b + 0.5 + delta_dim->y)) * iptr->sl);
-		*((int *)addr) = (color_lerp(c[0], c[1], progress));
-		c[3]++;
-	}
-}
-
 
 void					render_iso(const t_board *board, const t_imgdata *iptr,
 									t_dims *delta)
@@ -126,18 +95,9 @@ void					render_iso(const t_board *board, const t_imgdata *iptr,
 	{
 		while (--its.x >= 0)
 		{
-			line_to(board->vertex[its.y][its.x],
-					board->vertex[its.y][its.x + 1], board, iptr, delta, scale);
-			line_to(board->vertex[its.y][its.x],
-					board->vertex[its.y + 1][its.x], board, iptr, delta, scale);
-			line_to(board->vertex[its.y][its.x],
-					board->vertex[its.y + 1][its.x + 1],
-					board, iptr, delta, scale);
-					/*
             line_to(its, RIG, board, iptr, delta, scale);
 			line_to(its, BOT, board, iptr, delta, scale);
 			line_to(its, DIA, board, iptr, delta, scale);
-        */
 		}
 		its.x = max.x;
 	}
